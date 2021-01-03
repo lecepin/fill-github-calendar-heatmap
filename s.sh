eae13d8d880e9a14edf75d63bd866d5104d227e2
lecepin <383810086@qq.com>
2021-01-03 08:08:00 +0800

    11868

--- s.sh
+++ s.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/env sh
-echo "$(date)" > s.txt
+#!/bin/bash
 
-git add .
-git commit -m "${RANDOM}"
-git push -u origin main
+day=2
+
+while ((day<31))
+do
+	((day++))
+    _day=`printf "%02d\n" $day`    
+    date "01${_day}080821"
+
+    echo "$(date)" > s.txt
+
+    git add .
+    git commit -m "${RANDOM}"
+
+    sleep 2s
+done
+
+
+# git push -u origin main
