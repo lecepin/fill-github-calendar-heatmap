2668fe35702aa4e08120b3837196e8f235abb798
lecepin <383810086@qq.com>
2022-06-01 08:08:00 +0800

    14521

--- s.sh
+++ s.sh
@@ -2,11 +2,11 @@
 
 day=0
 
-while ((day<31))
+while ((day<30))
 do
 	((day++))
     _day=`printf "%02d\n" $day`    
-    date "05${_day}080822"
+    date "06${_day}080822"
 
     echo "$(date)" > s.txt
 
