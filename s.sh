b70a2d5af9c6eb6cda3233a64111d7ca0b501986
lecepin <383810086@qq.com>
2022-09-01 00:01:00 +0800

    090100012022 https://github.com/lecepin/fill-github-calendar-heatmap

--- s.sh
+++ s.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-day=0
+if [[ $# -lt 2  ]]; then
+    echo "必须提供开始时间和结束时间两个参数，如：$0 2022-01-01 $(date +%Y-%m-%d)"
+    exit 1
+fi
 
-while ((day<30))
-do
-    ((day++))
-    _day=`printf "%02d\n" $day`    
-    date "08${_day}080822"
+trap resetTime 0
 
-    echo "$(date)" > s.txt
+START_DAY=$(date -jf "%Y-%m-%d" "$1" "+%s")
+END_DAY=$(date -jf "%Y-%m-%d" "$2" "+%s")
 
-    git add .
-    git commit -m "${RANDOM}"
+resetTime() {
+    # 恢复系统时间
+    sntp -sS time.apple.com
+}
 
-    # sleep 1
-done
+modify() {
+    echo "处理中……"
+    while (( "${START_DAY}" <= "${END_DAY}" )); do
+        # [[[mm]dd]HH]MM[[cc]yy][.ss]
+        cur_day=$(date -r ${START_DAY} +"%m%d%H%M%Y")
+        START_DAY=$((${START_DAY}+86400))
+        
+        # 修改系统时间
+        date "${cur_day}"
+        # 修改内容
+        commit="${cur_day} https://github.com/lecepin/fill-github-calendar-heatmap"
+        echo $commit > log.txt
+        # 提交
+        git add .
+        git commit -m "${commit}"
+        
+        #sleep 1
+    done
+    
+    echo "处理完成"
+}
 
+modify
 
-# git push -u origin main
+exit 0
